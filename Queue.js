995c93b2336ca581b20ea95306c96f0d0b927cda
Younis Ereiqat <46542510+yuunji@users.noreply.github.com>
2019-08-07 19:35:55 -0700

    Update Queue.js

--- Queue.js
+++ Queue.js
@@ -1,1 +1,46 @@
+class Queue
+{
+  constructor()
+   {
+    this.items = [];
+   }
 
+
+  enqueue(data)
+  {
+  this.items.push(data)
+  }
+
+
+  dequeue()
+  {
+    if(this.isEmpty())
+    {
+      return("The Queue is Empty")
+    }
+    else
+    {
+      return this.items.shift()
+    }
+  }
+
+  front()
+  {
+    if(this.isEmpty())
+    {
+      return "No data in the Queue"
+    }
+    else
+    {
+      this.items[0]
+    }
+  }
+
+
+
+  isEmpty()
+  {
+    return this.items.length === 0
+  }
+  
+}
