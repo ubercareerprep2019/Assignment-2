a28a1bf363b55e8e5236467e4fd422a463e1a49b
Younis Ereiqat <46542510+yuunji@users.noreply.github.com>
2019-08-07 19:01:36 -0700

    Update part1.js

--- part1.js
+++ part1.js
@@ -1,1 +1,15 @@
-
+function PrintInOrder(Root)
+{
+  Treenode = Root
+  function PrintHelper(TreeNode)
+  {
+    if (TreeNode === None)
+    {
+      return([]);
+    };
+    else
+    {
+      return( PrintHelper(TreeNode.left) + [TreeNode.data] + PrintHelper(TreeNode.right) );
+    };
+   };
+};
