cbf2d2be92636deb0c44632ffdf89142301c698a
Younis Ereiqat <46542510+yuunji@users.noreply.github.com>
2019-08-12 19:43:44 -0700

    Update part1.js

--- part1.js
+++ part1.js
@@ -1,18 +1,26 @@
-//Ex1
 
 
-function PrintInOrder(Root)
-{
-  Treenode = Root
-  function PrintHelper(TreeNode)
-  {
-    if (TreeNode === None)
-    {
-      return([]);
-    }
-    else
-    {
-      return( PrintHelper(TreeNode.left) + [TreeNode.data] + PrintHelper(TreeNode.right) );
-    }
-   }
-}
+
+
+
+
+levelOrder() {
+      let result = [];
+      let Q = []; 
+      if (this.root != null) {
+          Q.push(this.root);
+          while(Q.length > 0) {
+              let node = Q.shift();
+              result.push(node.data);
+              if (node.left != null) {
+                  Q.push(node.left);
+              };
+              if (node.right != null) {
+                  Q.push(node.right);
+              };
+          };
+          return result;
+      } else {
+          return null;
+      };
+  };
