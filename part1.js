90dc7b50dc56d1b0db823cd9491e6e7f605b4439
Younis Ereiqat <46542510+yuunji@users.noreply.github.com>
2019-08-12 19:45:54 -0700

    Update part1.js

--- part1.js
+++ part1.js
@@ -1,7 +1,41 @@
 
+//PART 1 - TREES - Ex1
+class TreeNode {
+  constructor(data, left = null, right = null){
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+
+inOrder(){
+if (this.root === null){
+    return null;
+}
+else {
+  var result = new Array();
+  function traverseInOrder(node){
+    node.left && traverseInOrder(node.left);
+    result.push(node.data);
+    node.right && traverseInOrder(node.right);
+  }
+  traverseInOrder(this.root);
+  return result;
+}
+}
+
+
+}
 
 
 
+//PART 1 - TREES - Ex2
 
 
 levelOrder() {
