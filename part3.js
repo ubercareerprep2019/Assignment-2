816c631c81dbb6848bded182efb96cd040cb31b0
Younis Ereiqat <46542510+yuunji@users.noreply.github.com>
2019-08-19 18:41:09 -0700

    Update part3.js

--- part3.js
+++ part3.js
@@ -1,40 +1,85 @@
 import Queue from Queue;
 
 
-//Ex2 DFS
+//PART 3 - GRAPHS - Ex1
+class Graph {
 
 
-//Ex3 BFS
-
-
-function bfs(graph, root) {
-  var nodesLen = {};
-  
-  for (var i = 0; i < graph.length; i++) {
-    nodesLen[i] = Infinity;
+  constructor(VerticeCount){
+    this.VerticeCount = VerticeCount;
+    this.AdjList = new Map();
   }
-  nodesLen[root] = 0; 
-  
-  var queue = [root]; 
-  var current; 
 
-  while (queue.length != 0) {
-    current = queue.shift();
-    
-    var curConnected = graph[current];
-    var neighborIdx = []; 
-    var idx = curConnected.indexOf(1); 
-    while (idx != -1) {
-      neighborIdx.push(idx); 
-      idx = curConnected.indexOf(1, idx + 1); 
+
+  addVertex(v){
+    this.AdjList.set(v, []);
+  }
+
+
+  addEdge(v, w){
+    this.AdjList.get(v).push(w);
+    this.AdjList.get(w).push(v);
+  }
+
+  
+  
+  
+//PART 3 - GRAPHS - Ex2
+  bfs(start)
+  {
+    var visited = []
+    for(i = 0; i < this.VerticeCount; i ++){
+      visited[i] = false;
     }
-    
-    for (var j = 0; j < neighborIdx.length; j++) {
-      if (nodesLen[neighborIdx[j]] == Infinity) {
-        nodesLen[neighborIdx[j]] = nodesLen[current] + 1;
-        queue.push(neighborIdx[j]); 
+
+    var q = new Queue();
+    visited[start] = true;
+    q.enqueue(start);
+
+    while(!q.isEmpty()){
+       var QueueElement = q.dequeue()
+
+       console.log(QueueElement)
+       getlist = this.AdjList.get(QueueElement)
+
+       for(i in getlist){
+         var neighbor = getlist[i]
+
+         if(!visited[i]){
+           visited[neighbor] = true
+           q.enqueue(neighbor)
+         }
+       }
+
+    }
+  }
+
+
+  
+//PART 3 - GRAPHS - Ex3
+  dfs(start){
+    var visited = []
+    for(i = 0; i < this.VerticeCount; i++){
+      visited[i] = false;
+    }
+
+    this.DFSHelper(start, visited)
+  }
+
+  DFSHelper(vert, visited){
+    visited[vert] = true;
+    console.log(vert)
+    var neighbors = this.AdjList.get(vert)
+
+    for(i in neighbors){
+      var elem = neighbors[i]
+
+
+      if(!visited[elem]){
+        this.DFSHelper(elem, visited)
       }
     }
   }
-  return nodesLen;
-};
+
+
+}
